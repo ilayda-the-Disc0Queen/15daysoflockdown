a439e7759298710312525bd187892bcc525a8509
Ilayda <ilayda.bakare@gmail.com>
2020-12-12 16:33:15 +0000

    added about button

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './stylesheets/Navbar.css';
+import aboutBtn from './images/about-button.png';
 
 const Navbar = () => {
   return (
     <div className="ui grid navbar">
       <div class="sixteen wide column"></div>
-        <div class="fifteen wide column">
+        <div class="thirteen wide column">
           <a className="item" href="">Fifteen days of Lockdown</a>
         </div>
-        <div class="one wide column">
-          <a id="about" className="item" href="">About</a>
+        <div class="two wide column">
+          <a className="item" href="">
+            <img id="about" src={aboutBtn} alt="About button"/>
+          </a>
         </div>
         <div class="fifteen wide column"></div>
         <div class="one wide column">
